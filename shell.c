91d0efff5b54c82343ed582928ba545d62492860
Vasu0521c <vasudevanoffline@hotmail.com>
2026-07-07 01:43:49 +0530

    Fully functionable mini shell

--- shell.c
+++ shell.c
@@ -17,25 +17,41 @@ struct directory{
 directory* create_directory() {
 
 	directory *created_directory = malloc(sizeof(directory));
-	created_directory -> root_dir   = NULL;
+	created_directory -> root_dir   = NULL; /*  */
 	created_directory -> next_dir   = NULL;
 	created_directory -> child_dir  = NULL;
 	created_directory -> parent_dir = NULL;
 	created_directory -> is_file    = 0;
+    return created_directory;
 }
+
+typedef enum{
+    LS,
+    PWD,
+    WHOAMI,
+    MKDIR,
+    RM,
+    TOUCH,
+    MV,
+    /* CLEAR, */
+    CD,
+    /* CP, */
+    CAT,
+    ERROR
+} function_table;
 
 void list_directory_contents(directory *dir);
 void print_current_directory(directory *dir);
 void who_am_i();
 void make_directory(directory *dir, char *str);
 void remove_file(directory *dir, char *str);
-void touch(directory *dir, char *str);
+void touch_file(directory *dir, char *str);
 void touch_with_data(directory *dir, char *str, char *input);
-void move_or_rename(directory *dir, char *str_a, char *str_b);
-void clear();
+void move_or_rename(directory *dir, char *str);
+void clear_screen();
 directory* change_directory(directory *dir, char *str);
 void copy();
-void cat(directory *dir, char *file_name);
+void cat_file(directory *dir, char *file_name);
 
 int compare(char *str_a,char *str_b);
 
@@ -59,7 +75,7 @@ int str_contains(char *str, char target) {
 	return 0;
 }
 
-void cat(directory *dir, char *filename) {
+void cat_file(directory *dir, char *filename) {
 
 	directory *temp = dir -> child_dir;
 	if(temp == NULL) {
@@ -102,13 +118,38 @@ int compare(char* str_a, char* str_b) {
 	return 1;
 }
 
+char* nested_dir_changer(directory **dir, char *str) {
+
+    char arr[100][100];
+    int i = 0, j = 0,k = 0;
+    while(str[0] != '\n' && str[0] != '\0') {
+        if(!str_contains(str, '/'))
+            break;
+        while(str[0] != '/' && str[0] != '\n' && str[i] != '\0') {
+            arr[k][j] = str[0];
+            i++;
+            j++;
+            str++;
+        }
+        str++;
+        *dir = change_directory(*dir, arr[k]);
+        j = 0;
+        k++;
+        i++;
+    }
+    return str;
+}
+
 directory* change_directory(directory *dir, char *str) {
 
+    if(str_contains(str, '/') && get_length(str) > 1) {
+        str = nested_dir_changer(&dir, str);
+    }
 	if(compare(str, "/") == 1) {
 		while(dir -> parent_dir != NULL) {
 			dir = dir -> parent_dir;
-			return dir;
 		}
+		return dir;
 	}
 	directory *temp = dir -> child_dir;
 	while(compare(temp -> name, str) != 1) {
@@ -148,13 +189,47 @@ void print_current_directory(directory *dir) {
 	printf("\n%s\n", dir -> name);
 }
 
-void whoami() {
+void who_am_i() {
 	
 	printf("root\n");
 }
 
 void make_directory(directory *dir, char *Name) {
 
+    directory *constant = dir;
+    if(str_contains(Name, ' ')) {
+        char arr[20][80];
+        int i = 0;
+        int j = 0;
+        int x = 0;
+        while(Name[i] != '\n' && Name[i] != '\0') {
+            while(Name[i] != ' ' && Name[i] != '\n' && Name[i] != '\0') {
+                arr[x][j] = Name[i];
+                Name++;
+                j++;
+            }
+            Name++;
+            arr[x][j] = '\0';
+            j++;
+            if(str_contains(arr[x], '/')) {
+                char *ab = nested_dir_changer(&dir, arr[x]);
+                make_directory(dir, ab);
+                dir = constant;
+            }
+            else {
+                dir = constant;
+                make_directory(dir, arr[x]);
+            }
+            x++;
+        }
+        return;
+    }
+    else if(str_contains(Name, '/')) {
+        char *ab = nested_dir_changer(&dir, Name);
+        make_directory(dir, ab);
+        dir = constant;
+        return;
+    }
 	if(dir -> child_dir == NULL) {
 		dir -> child_dir               = create_directory();
 		dir -> child_dir -> parent_dir = dir;
@@ -174,6 +249,40 @@ void make_directory(directory *dir, char *Name) {
 
 void remove_file(directory *dir, char *str) {
 
+    directory *constant = dir;
+    if(str_contains(str, ' ')) {
+        char arr[20][80];
+        int i = 0;
+        int j = 0;
+        int x = 0;
+        while(str[i] != '\n' && str[i] != '\0') {
+            while(str[i] != ' ' && str[i] != '\n' && str[i] != '\0') {
+                arr[x][j] = str[i];
+                str++;
+                j++;
+            }
+            str++;
+            arr[x][j] = '\0';
+            j++;
+            if(str_contains(arr[x], '/')) {
+                char *ab = nested_dir_changer(&dir, arr[x]);
+                remove_file(dir, ab);
+                dir = constant;
+            }
+            else {
+                dir = constant;
+                remove_file(dir, arr[x]);
+            }
+            x++;
+        }
+        return;
+    }
+    else if(str_contains(str, '/')) {
+       char *ab = nested_dir_changer(&dir, str);
+        remove_file(dir, ab);
+        dir = constant;
+        return;
+    }
 	directory *temp = dir -> child_dir;
 	directory *prev = temp;
 	while(compare(temp -> name, str) != 1) {
@@ -200,8 +309,42 @@ void remove_file(directory *dir, char *str) {
 	return;
 }
 
-void touch(directory *dir, char *str) {
+void touch_file(directory *dir, char *str) {
 	
+    directory *constant = dir;
+    if(str_contains(str, ' ')) {
+        char arr[20][80];
+        int i = 0;
+        int j = 0;
+        int x = 0;
+        while(str[i] != '\n' && str[i] != '\0') {
+            while(str[i] != ' ' && str[i] != '\n' && str[i] != '\0') {
+                arr[x][j] = str[i];
+                str++;
+                j++;
+            }
+            str++;
+            arr[x][j] = '\0';
+            j++;
+            if(str_contains(arr[x], '/')) {
+                char *ab = nested_dir_changer(&dir, arr[x]);
+                touch_file(dir, ab);
+                dir = constant;
+            }
+            else {
+                dir = constant;
+                touch_file(dir, arr[x]);
+            }
+            x++;
+        }
+        return;
+    }
+    else if(str_contains(str, '/')) {
+        char *ab = nested_dir_changer(&dir, str);
+        touch_file(dir, ab);
+        dir = constant;
+        return;
+    }
 	if(dir -> child_dir == NULL) {
 		dir -> child_dir               = create_directory();
 		dir -> child_dir -> parent_dir = dir;
@@ -242,94 +385,107 @@ void touch_with_data(directory *dir, char *str, char *input) {
 	return;
 }
 
-void move_or_rename(directory *dir, char *str_a, char *str_b) {
+void move_or_rename(directory *dir, char *str) {
 
-	directory *c_name = dir;
-	directory *temp   = dir -> child_dir;
-	directory *prev   = temp;
-	while(compare(temp -> name, str_a) != 1) {
-		if(temp != NULL) {
-			prev = temp;
-			temp = temp -> next_dir;
-		}
-		else
-			return;
-	}
-	str_copy(temp -> name, str_b);
 }
 
 void test();
 
 int main() {
-	
-	directory *dir = create_directory();
+
+	directory *dir          = create_directory();
+    function_table commands;
 	str_copy(dir -> name, "root");
 	char input[500];
-	char arguments[100][100];
+    char function_name[20];
+    char arguments[100];
 	while(1) {
-		printf("%s$ ",dir -> name);
+		printf("%s $ ",dir -> name);
 		fgets(input, sizeof(input), stdin);
 		int i = 0;
-		int j = 0;
-		int k = 0;
-		while(input[i] != '\n' && input[i] != '\0') {
-			while(input[i] != ' ' && input[i] != '\0' && input[i] != '\n') {
-				arguments[j][k] = input[i];
-				i++;
-				k++;
-			}
-			arguments[j][k] = '\0';
-			k = 0;
-			i++;
-			j++;
-		}
+        int j = 0;
+        while(input[i] != ' ' && input[i] != '\n' && input[i] != '\0') {
+            function_name[i] = input[i];
+            i++;
+        }
+        function_name[i] = '\0';
+        i++;
+        while(input[i] != '\n' && input[i] != '\0') {
+            arguments[j] = input[i];
+            j++;
+            i++;
+        }
+        arguments[j] = '\0';
+        j++;
+
 		i = 0;
-		if(compare(arguments[i], "mkdir")) {
-			while(i < j - 1) {
-				++i;
-				make_directory(dir, arguments[i]);
-			}
-		}
-		else if(compare(arguments[i], "ls")) {
-			list_directory_contents(dir);
-		}
-		else if(compare(arguments[i], "pwd"))
-			print_current_directory(dir);
-		else if(compare(arguments[i], "whoami"))
-			whoami();
-		else if(compare(arguments[i], "touch")) {
-			if(str_contains(arguments[i + 2], '"') == 1) {
-				touch_with_data(dir, arguments[i + 1], arguments[i + 2]);
-				continue;
-			}
-			while(i < j - 1) {
-				i++;
-				touch(dir, arguments[i]);
-			}
-		}
-		else if(compare(arguments[i], "rm")) {
-			while(i < j - 1) {
-				i++;
-				remove_file(dir, arguments[i]);
-			}
-		}
-		else if(compare(arguments[i], "mv"))
-			move_or_rename(dir, arguments[1], arguments[2]);
-		else if(compare(arguments[i], "cd")) {
-			while(i < j - 1) {
-				i++;
-				dir = change_directory(dir, arguments[i]);
-			}
-		}
-		else if(compare(arguments[i], "cat"))
-			cat(dir, arguments[i + 1]);
-		else if(compare(arguments[i], "exit"))
-			exit(1);
-		else
-			printf("enter valid command\n");
+        if(compare(function_name, "ls"))
+            commands = LS;
+        else if(compare(function_name, "pwd"))
+            commands = PWD;
+        else if(compare(function_name, "whoami"))
+            commands = WHOAMI;
+        else if(compare(function_name, "mkdir"))
+            commands = MKDIR;
+        else if(compare(function_name, "rm"))
+            commands = RM;
+        else if(compare(function_name, "touch"))
+            commands = TOUCH;
+        else if(compare(function_name, "mv"))
+            commands = MV;
+        /* else if(compare(function_name, "clear")) */
+        /*     commands = CLEAR; */
+        else if(compare(function_name, "cd"))
+            commands = CD;
+        /* else if(compare(function_name, "cp")) */
+        /*     commands = CP; */
+        else if(compare(function_name, "cat"))
+            commands = CAT;
+        else if(compare(function_name, "exit"))
+            commands = ERROR;
 
-
-	}
+        switch(commands) {
+            case LS:
+                list_directory_contents(dir);
+                break;
+            case PWD:
+                print_current_directory(dir);
+                break;
+            case WHOAMI:
+                who_am_i();
+                break;
+            case MKDIR:
+                make_directory(dir, arguments);
+                break;
+            case RM:
+                remove_file(dir, arguments);
+                break;
+            case TOUCH:
+                touch_file(dir, arguments);
+                break;
+            case MV:
+                move_or_rename(dir, arguments);
+                break;
+            /* case CLEAR: */
+            /*     clear_screen(); */
+            /*     break; */
+            case CD:
+                dir = change_directory(dir, arguments);
+                break;
+            /* case CP: */
+            /*     copy(); */
+            /*     break; */
+            case CAT:
+                cat_file(dir, arguments);
+                break;
+            case ERROR:
+                exit(1);
+                break;
+            default:
+                printf("Enter valid Command:");
+                break;
+        }
+    }
 	test();
 	return 0;
 }
@@ -365,14 +521,14 @@ void test() {
 	}	else {
 		printf("remove test 2 passed\n");
 	}
-	touch(dir, "Anything_file");
+	touch_file(dir, "Anything_file");
 	if(dir -> child_dir == NULL) {
 		printf("touch test 1 failed\n");
 		exit(1);
 	}	else {
 		printf("touch test 1 passed\n");
 	}
-	move_or_rename(dir, "Anything_file", "Renamed_file");
+	move_or_rename(dir, "Anything_file Renamed_file");
 	if(compare(dir -> child_dir -> name, "Renamed_file") != 1) {
 		printf("Rename test 1 failed\n");
 		exit(1);
